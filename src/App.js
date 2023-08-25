db8e38416d6188f1108819aed80ec7748ea1ad50
Spring IT <spring@okvip.com>
2023-08-25 13:28:25 +0700

    democode

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+
 import { useDispatch, useSelector } from 'react-redux';
 import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
+import MyComponent from './components/MyComponent';
+
 const App = () => {
   const count = useSelector(state => state.counter.count);
   const dispatch = useDispatch();
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Em Minh học Reactjs Ahihi 1+1= ???
-        </p>
-        <div>Count = {count}</div>
-        <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-        <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-      </header>
+    <div>
+      Em Minh học Reactjs Ahihi
+      <MyComponent></MyComponent>
     </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Em Minh học Reactjs Ahihi 1+1= ???
+    //     </p>
+    //     <div>Count = {count}</div>
+    //     <button onClick={() => dispatch(increaseCounter())}>Increase</button>
+    //     <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
+    //   </header>
+    // </div>
   );
 }
 
---
+++ src/components/MyComponent.js
@@ -0,0 +1,21 @@
+// class component
+// funciton component
+
+import React from "react";
+class MyComponent extends React.Component {
+
+    //JSX
+    render() {
+        return (
+
+            <div> my first component
+
+                {Math.random()}
+            </div>
+
+        );
+
+    }
+}
+
+export default MyComponent;
